b56d11ecc41cb4e6430bf694a9dc4bfd95199be5
DK\mao <mao@sitecore.net>
2017-05-09 18:07:56 +0300

    Switch fallback language in the job pipeline.

---
+++ src/Sitecore.Support.103362/Sitecore/Jobs/JobRunner.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Data.LanguageFallback;
+using Sitecore.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Jobs
+{
+    public class JobRunner
+    {
+        public static void EnterLanguageFallbackSwitcher(JobArgs args)
+        {
+            if (JobRunner.CheckSwitchLanguage(args))
+            {
+                LanguageFallbackItemSwitcher.Enter(false);
+            }
+        }
+
+        public static void ExitLanguageFallbackSwitcher(JobArgs args)
+        {
+            if (JobRunner.CheckSwitchLanguage(args))
+            {
+                LanguageFallbackItemSwitcher.Exit();
+            }
+        }
+
+        private static bool CheckSwitchLanguage(JobArgs args)
+        {
+            return (args.Job.Category == "Install" && args.Job.Name == "Install")
+                || (args.Job.Category == "PackageDesigner" && args.Job.Name == "BuildPackage");
+        }
+    }
+}
